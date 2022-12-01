b8546198b7ec0170a3bb9735c75dc3868ac952e7
wjebalia <wiem.jebalia@gmail.com>
2022-12-01 18:04:07 +0100

    first commit

---
+++ src/main/java/com/demoseries/designpattern/demoseries/BatchTutorielApplication.java
@@ -0,0 +1,13 @@
+package com.demoseries.designpattern.demoseries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchTutorielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchTutorielApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demoseries/designpattern/demoseries/BatchTutorielApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoseries.designpattern.demoseries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchTutorielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
